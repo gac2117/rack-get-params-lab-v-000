d563d8fc596b4be7a5fd7cb82226ba4c60798871
Grace Choi <oiograceoio@gmail.com>
2019-03-26 16:28:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,14 +14,9 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
-    end
-
-    if req.path.match(/cart/)
+    elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
-      end
     else
       resp.write "Your cart is empty"
     end
